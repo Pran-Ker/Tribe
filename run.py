500dd4de3509d73ac90330e0889185e8f683280e
Saransh Mehta <saranshmehta96@gmail.com>
2020-06-21 17:45:43 +0530

    CORS

--- run.py
+++ run.py
@@ -13,8 +13,10 @@ import random
 from pandas import DataFrame
 import pandas as pd
 from flask import Flask , jsonify ,request
+from flask_cors import CORS, cross_origin
 
 app =Flask(__name__)
+CORS(app)
 
 """
 
